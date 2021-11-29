15c51092798743c267650c3a8431f882ccf09bb9
TabbyCatPie <78999461+TabbycatPie@users.noreply.github.com>
2021-11-29 17:23:06 +0800

    Update DangdangAnalizer.py
    finish dangdang.com bestseller from recent 30 days scrapper

--- DangdangAnalizer.py
+++ DangdangAnalizer.py
@@ -4,6 +4,10 @@ import codecs
 import json
 import urllib
 import urllib.request
+from bs4 import BeautifulSoup
+import lxml
+import time
+
 
 class DangdangSpider(object):
     def __init__(self):
@@ -13,7 +17,71 @@ class DangdangSpider(object):
         self.csvPath = r'./csv/dangdang'
         self.csvName = 'dangdang.csv'
 
+    def getHtmlSoup(self,url):
+        html = urllib.request.urlopen(url)
+        soup = BeautifulSoup(html.read(),'lxml')
+        return soup
+
+    def getBookinfo(self,book):
+        try:
+            mybook = {}
+            div = book.find_all('div',class_ = 'name')
+            bookname = div[0].find_all('a')[0]['title']
+            mybook['name'] = bookname
+            return mybook
+        except Exception as e:
+            print('getBookinfo() Error:')
+            print(e)
+            return None 
+
+    def printBookinfo(self,mybook):
+        mystr = ''
+        mystr = mystr + 'Name     :' + mybook['name'] + '\n'
+        return mystr
+    
+    def getTotalPage(self,soup):
+        try:
+            divs = soup.find_all('div',class_ = 'data')
+            pagestr = divs[0].find_all('span')[1].string
+            pagestr = pagestr.replace('/','')
+            print('Total pages  :' + pagestr)
+            return int(pagestr)
+        except Exception as e:
+            print("Can not get page info from url.")
+            print(e)
+            return 0
+
+    def run(self):
+        #get total page num
+        url_base = 'http://bang.dangdang.com/books/bestsellers/01.00.00.00.00.00-recent30-0-0-1-'
+        url = url_base + '1'
+        soup = self.getHtmlSoup(url)
+        total_page = self.getTotalPage(soup)
+        #get all books and out put
+        book_found = 0
+        for i in range(1,total_page):
+            print('Get html from:'+ url)
+            try:
+                ullist = soup.find('ul',attrs = {'class':'bang_list clearfix bang_list_mode'})
+                booklist = ullist.find_all('li')
+                for book in booklist:
+                    book_found = book_found + 1
+                    try:
+                        print(str(book_found) + '.' + self.printBookinfo(self.getBookinfo(book)))
+                        print('*'*60)
+                    except Exception as e:
+                        book_found = book_found - 1
+                        continue
+            except Exception as e:
+                print('Can not get info from soap,Error:')
+                print(e)
+            #go to next page
+            i = i + 1
+            url = url_base + str(i)
+            #time.sleep(5) #aviod trigger anti-bot
+            soup = self.getHtmlSoup(url)
+
 
 if __name__ == '__main__':
-    main = DouyinJsonAnalizer()
+    main = DangdangSpider()
     main.run()
